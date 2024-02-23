a2925eb1ecc637488e50ab61e66b8381fe6a6ea2
revanthpinnenti <159000914+revanthpinnenti@users.noreply.github.com>
2024-02-23 12:17:40 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,8 @@ def mavenBulid()
 {
   sh 'mvn package'
 }
+
+def tomcatDepoly(jobname,ip,context)
+{
+        sh 'scp /var/lib/jenkins/workspace/${d777}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war'    
+}
